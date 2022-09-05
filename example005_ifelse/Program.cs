aa62e61eaa22667b482388884f060f0b1ca82754
olga <yavruyano90@mail.ru>
2022-09-05 23:14:00 +0300

    example005 ifelse

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВедите имя пользователя: ");
+String username = Console.ReadLine();
+
+if (username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
